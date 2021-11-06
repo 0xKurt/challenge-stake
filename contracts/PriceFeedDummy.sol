bc9fa65d6aa9a1c5fa33c875742f6c78f20ca751
Kurt <Kurt@Merbeth.io>
2021-11-06 12:05:28 +0100

    added comment

--- contracts/PriceFeedDummy.sol
+++ contracts/PriceFeedDummy.sol
@@ -1,5 +1,6 @@
 pragma solidity 0.8.2;
 
+// for local testing only
 contract PriceFeedDummy {
     function latestRoundData() external view returns (uint80, int256, uint256, uint256, uint80) {
         return (
